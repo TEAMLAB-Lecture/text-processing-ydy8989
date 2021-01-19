df3fa5180484118b144c3246b0c91d3ee30711d7
DoyeonYoon <38639633+ydy8989@users.noreply.github.com>
2021-01-19 18:17:00 +0900

    commits

--- text_processing.py
+++ text_processing.py
@@ -9,31 +9,9 @@ NLP에서 흔히하는 전처리는 소문자 변환, 앞뒤 필요없는 띄어
 
 
 def normalize(input_string):
-    """
-     인풋으로 받는 스트링에서 정규화된 스트링을 반환함
-     아래의 요건들을 충족시켜야함
-    * 모든 단어들은 소문자로 되어야함
-    * 띄어쓰기는 한칸으로 되어야함
-    * 앞뒤 필요없는 띄어쓰기는 제거해야함
+    normed_lst = input_string.lower().split()
 
-         Parameters:
-             input_string (string): 영어로 된 대문자, 소문자, 띄어쓰기, 문장부호, 숫자로 이루어진 string
-             ex - "This is an example.", "   EXTRA   SPACE   "
-
-         Returns:
-             normalized_string (string): 위 요건을 충족시킨 정규회된 string
-             ex - 'this is an example.'
-
-         Examples:
-             >>> import text_processing as tp
-             >>> input_string1 = "This is an example."
-             >>> tp.normalize(input_string1)
-             'this is an example.'
-             >>> input_string2 = "   EXTRA   SPACE   "
-             >>> tp.normalize(input_string2)
-             'extra space'
-    """
-    normalized_string = None
+    normalized_string = ' '.join(normed_lst)
     return normalized_string
 
 
@@ -58,5 +36,6 @@ def no_vowels(input_string):
             >>> tp.normalize(input_string2)
             ''W lv Pythn!'
     """
+    ''.join([i for i in input_string if i not in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']])
     no_vowel_string = None
     return no_vowel_string
